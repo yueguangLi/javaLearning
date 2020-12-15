9e8ae93427fe32abd52c089f2895cbc5bce38e3d
liyueguang <yueguang.li@ly.com>
2020-12-15 13:45:39 +0800

    刷题使用

---
+++ basics/src/main/java/com/yueguang/java/BasicsApplication.java
@@ -0,0 +1,18 @@
+package com.yueguang.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author yueguang
+ * @Date 2020/12/15
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class BasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsApplication.class);
+    }
+}
---
+++ basics/src/main/java/com/yueguang/java/basic/Basic1.java
@@ -0,0 +1,10 @@
+package com.yueguang.java.basic;
+
+/**
+ * @Description TODO
+ * @Author yueguang
+ * @Date 2020/12/15
+ * @Version 1.0
+ */
+public class Basic1 {
+}
